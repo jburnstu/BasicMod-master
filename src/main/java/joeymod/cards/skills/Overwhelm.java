e8860ddaeb15871106edab8d58b9ee5b7b0af452
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-07-15 12:43:09 +0100

    Working across multiple cards here. (2)

--- src/main/java/joeymod/cards/skills/Overwhelm.java
+++ src/main/java/joeymod/cards/skills/Overwhelm.java
@@ -1,41 +1,41 @@
 package joeymod.cards.skills;
 
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import joeymod.actions.ForgetAction;
 import joeymod.cards.AbstractSleeperCard;
 import joeymod.character.MySleeperPlayer;
-import joeymod.powers.ComaPower;
-import joeymod.powers.PsychicStormPower;
 import joeymod.util.CardStats;
 
-//Forgotten cards cost zero this turn. This turn, when you play a card, forget it.
-public class PsychicStorm extends AbstractSleeperCard {
-    public static final String ID = makeID(PsychicStorm.class.getSimpleName());
+//Urgent, gain x block, forget a card.
+public class Overwhelm extends AbstractSleeperCard {
+    public static final String ID = makeID(Overwhelm.class.getSimpleName());
     private static Object MyCharacter;
     private static final CardStats info = new CardStats(
             MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            3 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
     //These will be used in the constructor. Technically you can just use the values directly,
     //but constants at the top of the file are easy to adjust.
     private int magicNumber = 2;
+    private static final int BLOCK = 6;
+    private static final int UPG_BLOCK = 3;
 
-    public PsychicStorm() {
+    public Overwhelm() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
         this.forget = true;
         this.magicNumber = magicNumber;
+        this.urgent = true;
+        setBlock(BLOCK, UPG_BLOCK);
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new ApplyPowerAction(p,p,new LoseStrengthPower(p,this.magicNumber)));
-        addToBot(new ApplyPowerAction(p,p,new PsychicStormPower(p,this.magicNumber)));
-    }
+        addToBot(new GainBlockAction(p,this.block));
+        addToTop(new ForgetAction(1,false,false,false));
+        }
 }
