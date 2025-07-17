2d1348e1a5e1ae07cf91d0b9c3b1b7ed7bf61583
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-07-17 14:36:23 +0100

    Hard Reset set up

--- src/main/java/joeymod/cards/skills/HardReset.java
+++ src/main/java/joeymod/cards/skills/HardReset.java
@@ -1,6 +1,9 @@
 package joeymod.cards.skills;
 
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.utility.LoseBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -10,16 +13,16 @@ import joeymod.character.MySleeperPlayer;
 import joeymod.powers.ComaPower;
 import joeymod.util.CardStats;
 
-//Forgotten cards cost zero this turn. This turn, when you play a card, forget it.
+//Urgent. Lose all your block. Gain 2 energy. draw three cards.
 public class HardReset extends AbstractSleeperCard {
     public static final String ID = makeID(HardReset.class.getSimpleName());
     private static Object MyCharacter;
     private static final CardStats info = new CardStats(
             MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
-            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
             CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
-            3 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
     //These will be used in the constructor. Technically you can just use the values directly,
     //but constants at the top of the file are easy to adjust.
@@ -27,16 +30,14 @@ public class HardReset extends AbstractSleeperCard {
 
     public HardReset() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
-        this.forget = true;
         this.magicNumber = magicNumber;
+        this.urgent = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-
-        for (AbstractCard c : ((MySleeperPlayer) AbstractDungeon.player).forgottenPile.group) {
-            ((AbstractSleeperCard) c).backForgottenCard.setCostForTurn(0);
-            addToTop(new ApplyPowerAction(p, p, new ComaPower(p, this.magicNumber)));
-        }
+        addToBot(new LoseBlockAction(p,p,currentBlock)); //how to get this?
+        addToBot(new GainEnergyAction(2));
+        addToBot(new DrawCardAction(3));
     }
 }
