0fd95c843ea33dec36365f6a096631c93c36d4da
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-08-05 11:01:05 +0100

    Some new cards

---
+++ src/main/java/joeymod/cards/cardBeta/skills/WeakAllThenAmnesia.java
@@ -0,0 +1,41 @@
+package joeymod.cards.cardBeta.skills;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.watcher.PressEndTurnButtonAction;
+import com.megacrit.cardcrawl.actions.watcher.SkipEnemiesTurnAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+import joeymod.cards.AbstractSleeperCard;
+import joeymod.character.MySleeperPlayer;
+import joeymod.powers.AmnesiaPower;
+import joeymod.util.CardStats;
+
+//Urgent. end your turn and have another turn. exhaust
+public class WeakAllThenAmnesia extends AbstractSleeperCard {
+    public static final String ID = makeID(WeakAllThenAmnesia.class.getSimpleName());
+    private static Object MyCharacter;
+    private static final CardStats info = new CardStats(
+            MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.UNCOMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private int magicNumber = 2;
+
+    public WeakAllThenAmnesia() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+        this.exhaust = true;
+        this.urgent = true;
+        this.magicNumber = magicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(m,p, new WeakPower(m,this.magicNumber,false)));
+        addToBot(new ApplyPowerAction(p,p, new AmnesiaPower(p,this.magicNumber)));
+    }
+}
