8bc266bdde53ea9ccdebd730cc586975ade17f37
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-08-20 15:43:22 +0100

    Fixed coma card (at last!)

--- src/main/java/joeymod/cards/skills/Coma.java
+++ src/main/java/joeymod/cards/skills/Coma.java
@@ -1,10 +1,12 @@
 package joeymod.cards.skills;
 
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import joeymod.actions.FreeForgottenCardsAction;
 import joeymod.cards.AbstractSleeperCard;
 import joeymod.character.MySleeperPlayer;
 import joeymod.patches.AbstractCardBackForgottenCardPatch;
@@ -32,10 +34,8 @@ public class Coma extends AbstractSleeperCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new FreeForgottenCardsAction(p));
+        addToBot(new ApplyPowerAction(p, p, new ComaPower(p, this.magicNumber)));
 
-        for (AbstractCard c : ((MySleeperPlayer) AbstractDungeon.player).forgottenPile.group) {
-            AbstractCardBackForgottenCardPatch.backForgottenCard.get(c).setCostForTurn(0);
-            addToTop(new ApplyPowerAction(p, p, new ComaPower(p, this.magicNumber)));
-        }
     }
 }
