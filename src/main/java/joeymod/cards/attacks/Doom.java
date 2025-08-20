f16ab3a3741d90753a83ed39130d71d0f7ec3dc5
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-08-20 15:20:24 +0100

    Set up new card (doom) and envelop

---
+++ src/main/java/joeymod/cards/attacks/Doom.java
@@ -0,0 +1,44 @@
+package joeymod.cards.attacks;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import joeymod.cards.AbstractSleeperCard;
+import joeymod.character.MySleeperPlayer;
+import joeymod.powers.HealAtTurnStartPower;
+import joeymod.util.CardStats;
+
+// Attack and inflict woozy
+public class Doom extends AbstractSleeperCard {
+    public static final String ID = makeID(Doom.class.getSimpleName());
+    private static Object MyCharacter;
+    private static final CardStats info = new CardStats(
+            MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.ATTACK, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            2 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    private static final int DAMAGE = 25;
+    private static final int UPG_DAMAGE = 5;
+    private static int baseMagicNumber = 20;
+
+    public Doom() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+
+        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
+        this.magicNumber = baseMagicNumber;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DamageAction(m, new DamageInfo(p, damage, DamageInfo.DamageType.NORMAL), AbstractGameAction.AttackEffect.SLASH_VERTICAL));
+        addToBot(new ApplyPowerAction(m,p,new HealAtTurnStartPower(m,p,this.magicNumber)));
+          }
+}
+
--- src/main/java/joeymod/cards/skills/Envelop.java
+++ src/main/java/joeymod/cards/skills/Envelop.java
@@ -22,14 +22,10 @@ public class Envelop extends AbstractSleeperCard {
     );
     //These will be used in the constructor. Technically you can just use the values directly,
     //but constants at the top of the file are easy to adjust.
-    private static final int DAMAGE = 2;
-    private static final int UPG_DAMAGE = 2;
     private static final int baseMagicNumber = 1;
 
     public Envelop() {
         super(ID, info); //Pass the required information to the BaseCard constructor.
-
-        setDamage(DAMAGE, UPG_DAMAGE); //Sets the card's damage and how much it changes when upgraded.
         this.magicNumber = baseMagicNumber;
     }
 
@@ -39,7 +35,7 @@ public class Envelop extends AbstractSleeperCard {
 
     public void applyPowers() {
         int numberOfForgottenCards = ((MySleeperPlayer) AbstractDungeon.player).forgottenPile.size();
-        this.baseDamage = numberOfForgottenCards * this.magicNumber;
+        this.block = numberOfForgottenCards * this.magicNumber;
         super.applyPowers();
         this.rawDescription = cardStrings.DESCRIPTION + cardStrings.EXTENDED_DESCRIPTION[0];
         initializeDescription();
@@ -49,7 +45,6 @@ public class Envelop extends AbstractSleeperCard {
         this.rawDescription = cardStrings.DESCRIPTION;
         initializeDescription();
     }
-
 
     public void upgrade() {
         if (!this.upgraded) {
