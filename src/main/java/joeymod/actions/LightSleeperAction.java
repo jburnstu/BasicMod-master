9109b65ab47b166dfad7030d77028ae036027964
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-08-26 12:59:16 +0100

    New cards and actions

---
+++ src/main/java/joeymod/actions/LightSleeperAction.java
@@ -0,0 +1,28 @@
+package joeymod.actions;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.GameActionManager;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import joeymod.cards.ForgottenCard;
+
+public class LightSleeperAction extends AbstractGameAction {
+
+    public void update() {
+        AbstractDungeon.actionManager.addToTop((AbstractGameAction)new WaitAction(0.4F));
+        tickDuration();
+        if (this.isDone) {
+            for (AbstractCard c : DrawCardAction.drawnCards) {
+                if (!(c instanceof ForgottenCard)) {
+                    AbstractDungeon.player.hand.moveToDiscardPile(c);
+                    c.triggerOnManualDiscard();
+                    GameActionManager.incrementDiscard(false);
+                }
+            }
+        }
+    }
+}
---
+++ src/main/java/joeymod/cardBeta/powers/MaladaptiveDreaming.java
@@ -0,0 +1,51 @@
+package joeymod.cardBeta.powers;
+
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.BerserkPower;
+import joeymod.cards.AbstractSleeperCard;
+import joeymod.character.MySleeperPlayer;
+import joeymod.powers.DisseveredFormPower;
+import joeymod.powers.WoozyPower;
+import joeymod.util.CardStats;
+
+//Gain 4 woozy. At the start of your turn, gain 1 energy
+public class MaladaptiveDreaming extends AbstractSleeperCard {
+    public static final String ID = makeID(MaladaptiveDreaming.class.getSimpleName());
+    private static Object MyCharacter;
+    private static final CardStats info = new CardStats(
+            MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
+            CardType.POWER, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
+            CardRarity.RARE, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
+    );
+    //These will be used in the constructor. Technically you can just use the values directly,
+    //but constants at the top of the file are easy to adjust.
+    int baseMagicNumber = 4;
+    int magicUpgrade = -1;
+
+
+    public MaladaptiveDreaming() {
+        super(ID, info); //Pass the required information to the BaseCard constructor.
+        setMagic(baseMagicNumber,magicUpgrade);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new ApplyPowerAction(p, p, new WoozyPower(p,this.magicNumber)));
+        addToBot(new ApplyPowerAction(p,p,new BerserkPower(p,1)));
+    }
+
+    public void upgrade() {
+        if (!this.upgraded) {
+            upgradeName();
+            upgradeMagicNumber(1);
+        }
+    }
+
+    public AbstractCard makeCopy() {
+        return new MaladaptiveDreaming();
+    }
+}
