d2147ad61652994ee482c31dc3c9539e3d13d35b
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-08-27 14:27:22 +0100

    Corrected the targeting of all cards (most of the skills were aimed at "ENEMY"

--- src/main/java/joeymod/cards/skills/CopingMechanism.java
+++ src/main/java/joeymod/cards/skills/CopingMechanism.java
@@ -17,7 +17,7 @@ public class CopingMechanism extends AbstractSleeperCard {
             MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.SKILL, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.COMMON, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
             0 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
     //These will be used in the constructor. Technically you can just use the values directly,
