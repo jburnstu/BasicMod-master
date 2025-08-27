7eeefa977ac16d971862037e4c88a0ace0570f60
jburnstu <75335189+jburnstu@users.noreply.github.com>
2025-08-27 14:37:26 +0100

    Forgotten card now switch-cases targeting to handle the blind targeting of cards. appears to work!

--- src/main/java/joeymod/cards/ForgottenCard.java
+++ src/main/java/joeymod/cards/ForgottenCard.java
@@ -17,7 +17,7 @@ public class ForgottenCard extends AbstractSleeperCard {
             MySleeperPlayer.Meta.CARD_COLOR, //The card color. If you're making your own character, it'll look something like this. Otherwise, it'll be CardColor.RED or similar for a basegame character color.
             CardType.STATUS, //The type. ATTACK/SKILL/POWER/CURSE/STATUS
             CardRarity.SPECIAL, //Rarity. BASIC is for starting cards, then there's COMMON/UNCOMMON/RARE, and then SPECIAL and CURSE. SPECIAL is for cards you only get from events. Curse is for curses, except for special curses like Curse of the Bell and Necronomicurse.
-            CardTarget.SELF, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
+            CardTarget.ENEMY, //The target. Single target is ENEMY, all enemies is ALL_ENEMY. Look at cards similar to what you want to see what to use.
             1 //The card's base cost. -1 is X cost, -2 is no cost for unplayable cards like curses, or Reflex.
     );
     //These will be used in the constructor. Technically you can just use the values directly,
@@ -41,7 +41,24 @@ public class ForgottenCard extends AbstractSleeperCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         this.p.forgottenPile.group.remove(this.frontForgottenCard);
-        addToBot(new NewQueueCardAction(this.frontForgottenCard, m));
+
+        switch (this.frontForgottenCard.target) {
+            case ENEMY:
+                addToBot(new NewQueueCardAction(this.frontForgottenCard, m, true, true));
+                break;
+            case SELF:
+                addToBot(new NewQueueCardAction(this.frontForgottenCard, p, true, true));
+                break;
+            default:
+                addToBot(new NewQueueCardAction(this.frontForgottenCard, m, true, true));
+
+        }
+
+        if (this.frontForgottenCard.target == CardTarget.ENEMY) {
+            addToBot(new NewQueueCardAction(this.frontForgottenCard, m, true, true));
+        } else {
+            addToBot(new NewQueueCardAction(this.frontForgottenCard, false, true, true));
+        }
         if (this.frontForgottenCard instanceof AbstractSleeperCard) {
 //            this.p.hand.addToHand(this.forgottenCard);
             ((AbstractSleeperCard) this.frontForgottenCard).triggerOnRemembered(p,m,true);
